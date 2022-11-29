135ecbd023ac111e55dbcc585fb57c7302aefc39
Nazar Shiyka <nazar.shyika.ir.2022@lpnu.ua>
2022-11-29 17:04:41 +0200

    my first lab

---
+++ Lab1.h
@@ -0,0 +1,20 @@
+﻿
+
+#include <stdio.h>
+#include <math.h>
+/*my first project*/
+int main()
+{
+	int n,k,b;
+	b = 5;
+	for (n = 0; n < 10; n++)
+	{
+		k = 5 * b;
+		printf("%d", k);
+	}
+
+	
+
+
+		return 0;
+}
